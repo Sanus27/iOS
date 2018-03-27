91e29b0648492b37a34c9f798f8c27836caeb98a
Luis Castillo Z <luiscastillo@iwebsapp.com>
2018-03-27 00:35:53 -0600

    mensajes del paciente

--- Sanus/Sources/ChatClient/Controller/ChatController.swift
+++ Sanus/Sources/ChatClient/Controller/ChatController.swift
@@ -18,6 +18,7 @@ class ChatController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var ref:DocumentReference!
     var listItems = [Contact]()
     var uid: String = ""
+    private let model = ChatModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,45 +31,12 @@ class ChatController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     private func showData(){
 
-        self.getRef.collection("contactos").whereField("autor", isEqualTo: self.uid ).addSnapshotListener { (result, error) in
-            if let error = error {
-                print("se ha producido un error \(error)")
-            } else {
-
-                self.listItems.removeAll()
-                self.tableData.reloadData()
-                
-                for doc in result!.documents {
-                    let id = doc.documentID
-                    let valCont = doc.data()
-                    let author = valCont["autor"] as? String
-                    let doctor = valCont["doctor"] as? String
-
-
-                    self.ref = Firestore.firestore().collection("usuarios").document( doctor! )
-                    self.ref.addSnapshotListener { (resp, error) in
-                        if let error = error {
-                            print("se ha producido un error \(error)")
-                        } else {
-
-                            if let resp = resp {
-                                let valUser = resp.data()
-                                var avatar = valUser!["avatar"] as? String
-                                let name = valUser!["nombre"] as? String
-                                let lastname = valUser!["apellido"] as? String
-                                let estado = valUser!["estado"] as? String
-                                avatar = "gs://sanus-27.appspot.com/avatar/" + avatar!
-                                let contact = Contact( id:id, avatar:avatar!, autor:author!, doctor:doctor!, nombre:name!, apellidos: lastname!, estado:estado! )
-                                self.listItems.append(contact)
-                                self.tableData.reloadData()
-                            }
-
-                        }
-                    }
-
-                }
-            }
-        }
+        self.model.showData(getRef:getRef, completionHandler:  { resp in
+            self.listItems.removeAll()
+            self.tableData.reloadData()
+            self.listItems = self.model.listItems
+            self.tableData.reloadData()
+        })
         
     }
     
